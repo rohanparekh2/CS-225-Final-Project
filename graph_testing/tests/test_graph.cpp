e1770b16983d47fd9415c90d3b503e91a9d576af
Kaushal Dadi <kdadi2@siebl-0224-23.ews.illinois.edu>
2021-12-03 23:03:28 -0600

    All Test Cases Complete For Now

--- graph_testing/tests/test_graph.cpp
+++ graph_testing/tests/test_graph.cpp
@@ -1,21 +1,24 @@
 #include "catch2/catch.hpp"
 #include <fstream>
 #include <vector>
+#include "../include/edge.h"
+#include "../include/vertex.h"
+#include "../include/graph.h"
 
 TEST_CASE("test edge", "[weight=3]") {
-	Edge e(1.0,2.0,1);
+	Edge::Edge e(1.0,2.0,1);
 	REQUIRE(e.getVertexOne() == 1.0);
 	REQUIRE(e.getVertexTwo() == 2.0);
 	REQUIRE(e.getWeight() == 1);
 }
 
 TEST_CASE("test vertex", "[weight=1]") {
-	Vertex v(52.0);
+	Vertex::Vertex v(52.0);
 	REQUIRE(v.getEdges == 0);
 }
 
 TEST_CASE("test BFS", "[weight=1]") {
-	Graph g("../../data/Pokedex.csv");
+	Graph::Graph g("../../data/Pokedex.csv");
 	std::vector<std::string> r = {"Zacian", "Zamazenta", "Eternatus"};
 	REQUIRE(g.BSF(888) == r);
 }
